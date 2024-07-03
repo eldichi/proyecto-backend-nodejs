c200202e371c475dc89c25afd047190b04bee64a
hcastroman <163900041+hcastroman@users.noreply.github.com>
2024-07-03 02:31:29 -0300

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,18 +1,41 @@
 const mysql = require('mysql');
+//import mysql from 'mysql2' /* LOS MODULOS DE TERCEROS NO LLEVAN LA EXTESION .js */
+
+///const mysql = require("mysql2")
+///require('dotenv').config()
+///usando archivo .env:
+///IP='127.0.0.1'
+///PORT=3000
+///DB_USER="ffrontera"
+///DB_PWD="3zcaw5FF#"
+///DB_NAME="biblioteca"
+///SECRET_KET='123456'
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'placas'
+    
+    //host: "mysql-mvarela.alwaysdata.net", //o es https://phpmyadmin.alwaysdata.com/?
+    //user: "mvarela_admin", // alwaysdata no me deja  poner root, y obliga a que el usuario inicie con esos números
+    //password: "!test1234@",//alwaysdata no me deja  poner root
+    //database: "mvarela_sweetbite",//alwaysdata obliga a que el usuario inicie con esos números
+
+    ///host: process.env.DB_HOST,
+    ///user: process.env.DB_USER,
+    ///password: process.env.DB_PASS,
+    ///database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
     if (err) {
-        console.error('error en coneccion : ' + err.stack);
+        console.error('Error en coneccion a la DB: ' + err.stack);
         return;
     }
-    console.log('Base de datos Placas conectada ');
+    console.log('DB Placas conectada con exito');
 });
 
 module.exports = connection;
+//export default connection; /* asi se exporta */
