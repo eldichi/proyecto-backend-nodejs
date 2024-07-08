82e0bca8e2eadbddc7dd27045f0c3bd871b21f99
Hugo Castroman <163900041+hcastroman@users.noreply.github.com>
2024-07-07 21:08:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const app = express();
 
 const path = require("path");
 
-//Midleware para archivos estáticos y públicos
+//Middleware para archivos estáticos y públicos
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-app.use("/productos", require("./rutas/productosRutas"));
+const productosRoutes = require("./src/routes/productos.router");
+const authRoutes = require("./src/routes/auth.router");
+
+app.use('/productos',productosRoutes);
+app.use('/auth',authRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App funcionando en http://localhost:${PORT}`)
+});
